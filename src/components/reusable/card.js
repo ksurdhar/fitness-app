5831bc419a83e2c3b63ef61668e5b594e85d432e
Kiran Surdhar <ksurdhar1@gmail.com>
2018-06-22 19:49:59 -0600

    added carousel to demo page

--- src/components/reusable/card.js
+++ src/components/reusable/card.js
@@ -8,10 +8,16 @@ import {
   Text
 } from 'react-native'
 
+import SideSwipe from 'react-native-sideswipe'
+
 import { common, COLORS } from './styles'
 class Card extends React.Component {
   constructor() {
     super()
+
+    this.state = {
+      carouselIdx: 0
+    }
   }
 
   componentDidMount() {
@@ -23,18 +29,54 @@ class Card extends React.Component {
     this.props.isEnabled && this.props.onPress && this.props.onPress()
   }
 
+  renderCarouselIdx = (idx, item) => {
+    switch (idx) {
+      case 0:
+        return (
+          <Text>Hello Samm!</Text>
+        )
+        break
+      case 1:
+        return (
+          <Text>I wanna touch your butt!</Text>
+        )
+        break
+    }
+  }
+
+  toggleIdx = () => {
+    const newIdx = this.state.carouselIdx === 0 ? 1 : 0
+    this.setState({
+      carouselIdx: newIdx
+    })
+  }
+
   render() {
     const { width } = Dimensions.get('window')
 
     return (
-      <TouchableWithoutFeedback onPress={() => {console.log('you pressed me!')}}>
+      <TouchableWithoutFeedback onPress={this.toggleIdx}>
         <View style={styleCard(width)}>
           <View>
             <View style={{ borderBottomColor: COLORS.gray1, borderBottomWidth: 1, marginBottom: 10, top: 64, zIndex: 2}} />
             <View style={{ paddingLeft: 16, paddingRight: 16 }}>
               <Text style={[common.tajawal3, {fontSize: 18, color: COLORS.gray8}]}>{this.props.subHeader}</Text>
               <Text style={[common.tajawal5, {fontSize: 26, color: COLORS.gray10, paddingBottom: 10}]}>{this.props.header}</Text>
-              { this.props.children }
+              <SideSwipe
+                index={this.state.carouselIdx}
+                itemWidth={width}
+                style={{ width: width - 50 }}
+                data={[1,2]}
+                useNativeDriver={true}
+                onIndexChange={index => {
+                  this.setState(() => ({ carouselIdx: index }))
+                }}
+                renderItem={({ itemIndex, currentIndex, item, animatedValue }) => (
+                  <View style={[{ width: width - 20, marginRight: 20}]}>
+                    { this.renderCarouselIdx(itemIndex, item) }
+                  </View>
+                )}
+              />
             </View>
           </View>
         </View>
