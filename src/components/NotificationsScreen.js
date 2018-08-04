ed3dc232cdf620a72a82ba5369421352e6f1ff48
Kiran Surdhar <ksurdhar1@gmail.com>
2018-08-03 18:45:00 -0600

    added logic to toggle on and off notifications for workouts

--- src/components/NotificationsScreen.js
+++ src/components/NotificationsScreen.js
@@ -17,11 +17,8 @@ import { Feather } from '@expo/vector-icons'
 
 import { common } from './reusable/common'
 import KButton from './reusable/button'
+import Switch from './reusable/switch'
 import * as workoutActions from '../redux/actions/workoutActions'
-
-INITIAL_STATE = {
-  selectedWorkout: null,
-}
 
 const mapStateToProps = (state, ownProps) => {
   return {
@@ -39,25 +36,67 @@ class NotificationsScreen extends React.Component {
 
   constructor() {
     super()
-    this.state = INITIAL_STATE
+    this.state = {
+      notificationData: {} // object with workout id to notification
+    }
   }
 
-  resetState() {
-    this.setState(INITIAL_STATE)
+  addNotification = (workoutID) => {
+    console.log('add notification')
+    this.setState((prevState) => {
+      return produce(prevState, (draftState) => {
+        draftState.notificationData[workoutID] = new Date()
+      })
+    })
+  }
+
+  updateNotification = () => {
+    console.log('updating notification')
+  }
+
+  removeNotification = (workoutID) => {
+    console.log('remove notification')
+    this.setState((prevState) => {
+      return produce(prevState, (draftState) => {
+        draftState.notificationData[workoutID] = null
+      })
+    })
+  }
+
+  toggleNotification = (workoutID) => {
+    console.log('toggling', workoutID)
+    if (this.state.notificationData[workoutID]) {
+      this.removeNotification(workoutID)
+    } else {
+      this.addNotification(workoutID)
+    }
+  }
+
+  // here is where we grab the notifications endpoint and check if the toggles are on
+  componentDidMount() {
+    // console.log('mounted',this.props)
+    // const notificationData = {}
+    // this.props.workouts.for
+    // this.setState()
   }
 
   componentDidUpdate() {
+    console.log('cmp update', this.state)
     // console.log('PROMPT WORKOUT PROPS', this.props.workouts)
   }
 
-  // render each workout name
+  // if the switch is tapped, send a value to the server, change it with the time thing
   renderWorkouts = () => {
-    const workoutElements = this.props.workouts.map((workout) => {
+    const workoutElements = this.props.workouts.map((workout, idx) => {
       return (
-        <View style={[common.row, { marginTop: 20 }]}>
+        <View style={[common.row, { marginTop: 20, justifyContent: 'left' }]}>
           <Text style={[common.tajawal5, {fontSize: 22, color: COLORS.gray10, textAlign: 'center'}]}>
             { workout.name }
           </Text>
+          <Switch
+            enabled={this.state.notificationData[workout.id]}
+            onPress={() => this.toggleNotification(workout.id) }
+          />
         </View>
       )
     })
